157df5cc7eec3f3ef40cf9621a25cae3e9be29e7
Alex Robertson <lex1989@gmail.com>
2017-02-26 16:52:57 +0000

    Created tests for avatar

---
+++ test/email.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import {expect} from 'chai';
+import Email from '../lib/email';
+
+describe('<Email/>', function() {
+  it('should have an input for the eamil', function() {
+    
+  })
+})
